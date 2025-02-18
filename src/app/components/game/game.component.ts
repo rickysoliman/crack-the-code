ae15221ca4f9195b06dc7bbabfdf02d742c4c08f
Ricky Marasigan <rickysoliman@gmail.com>
2025-02-18 10:35:03 -0500

    global styling and form control

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
-
+  codeForm = new FormGroup({
+    digit1: new FormControl(''),
+    digit2: new FormControl(''),
+    digit3: new FormControl(''),
+    digit4: new FormControl(''),
+  });
 }
