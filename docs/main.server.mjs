8dab0c0510c032ddbf4f3935e8a6200686a4a429
Ricky Marasigan <rickysoliman@gmail.com>
2025-09-11 11:36:06 -0400

    new prod build defining base-href

--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-U67UJAHR.mjs";import"./chunk-ZGXJ6HMQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-YUP34YJQ.mjs";import"./chunk-ZGXJ6HMQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
